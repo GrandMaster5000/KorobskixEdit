b5bc71325e9380f112b8860aa65739efa81a1304
Tolik22869 <ggamer556@mail.ru>
2021-08-30 16:38:39 +0300

    add start create UI

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -3,15 +3,28 @@ import axios from 'axios';
 import "../../helpers/iframeLoader.js";
 import DOMHelper from '../../helpers/dom-helper.js';
 import EditorText from '../editorText';
+import UIkit from 'uikit';
+import Spinner from '../spinner/spinner.js';
 
 export default class Editor extends Component {
     constructor() {
         super();
         this.currentPage = 'index.html';
+        this.ref = (elem) => {
+            elem.querySelector('button.uk-button-primary').onclick = () => {
+                this.save(() => {
+                    UIkit.notification({message: 'Успешно сохранено', status: 'success'})
+                },
+                () => {
+                    UIkit.notification({message: 'Ошибка сохранения', status: 'danger'})
+                });
+            }
+        }
     }
     state = {
         pageList: [],
-        newPageName: ''
+        newPageName: '',
+        loading: true
     }
 
     componentDidMount() {
@@ -19,12 +32,12 @@ export default class Editor extends Component {
     }
 
     init(page) {
-        this.iframe = document.querySelector('iframe');
-        this.open(page);
+        this.iframe = document.querySelector('#iframe');
+        this.open(page, this.isLoaded);
         this.loadPageList();
     }
 
-    open(page) {
+    open(page, cb) {
         this.currentPage = page;
 
         axios
@@ -39,15 +52,20 @@ export default class Editor extends Component {
         .then(html => axios.post("./api/saveTempPage.php", {html}))
         .then(() => this.iframe.load("../temp.html"))
         .then(() => this.enableEditing())
-        .then(() => this.injectStyles());
+        .then(() => this.injectStyles())
+        .then(cb);
     }
 
-    save() {
+    save(onSuccess, onError) {
+        this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
         const html = DOMHelper.serializeDOMToString(newDom);
         axios
         .post('./api/savePage.php', {pageName: this.currentPage, html})
+        .then(onSuccess)
+        .catch(onError)
+        .finally(this.isLoaded);
     }
 
     enableEditing() {
@@ -95,11 +113,52 @@ export default class Editor extends Component {
         .catch(() => alert("Страница не существует!"));
     }
 
-    render() {
+    isLoading = () => {
+        this.setState({
+            loading: true
+        });
+    }
+
+    isLoaded = () => {
+        this.setState({
+            loading: false
+        })
+    }
+
+    render () {
+        const {loading} = this.state;
+        const modal = true;
+        let spinner;
+
+        loading ? spinner = <Spinner active/> : spinner = <Spinner />;
+
+
         return (
             <>
-                <button onClick={() => this.save()}></button>
-                <iframe src={this.currentPage} frameBorder="0"></iframe>
+                <iframe id='iframe' src={this.currentPage} frameBorder="0"></iframe>
+
+                {spinner}
+
+                <div className='panel'>
+                    <button uk-toggle="target: #modal-save" className="uk-button uk-button-primary">Опубликовать</button>
+                </div>
+                <div id="modal-save" uk-modal={modal.toString()}>
+                    <div className="uk-modal-dialog uk-modal-body" ref={this.ref}>
+                        <h2 className="uk-modal-title">Сохранение</h2>
+                        <p>Вы действительно хотите сохранить изменения?</p>
+                        <p className="uk-text-right">
+                            <button 
+                            className="uk-button uk-button-default uk-modal-close" 
+                            type="button"
+                            >Закрыть</button>
+                            <button 
+                            className="uk-button uk-button-primary uk-modal-close" 
+                            type="button"
+                            >Опубликовать</button>
+                        </p>
+                    </div>
+                </div>
+              
             </>
         )
     }
